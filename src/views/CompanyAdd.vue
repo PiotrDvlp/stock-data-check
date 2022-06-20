1870f758f60f75055ee4bdb53a3e8cac60b8d5b1
Piotr Strzelecki <strzelecki.piotr@protonmail.com>
2022-06-20 15:46:53 +0200

    feature: Add model to company form

--- src/views/CompanyAdd.vue
+++ src/views/CompanyAdd.vue
@@ -1,37 +1,52 @@
 <template>
-    <b-form @submit.prevent="addCompany">
-        <b-form-group
-            id="input-group-1"
-            label="Company symbol:"
-            label-for="company-symbol"
-            description="Provide the stock exchange symbol of a company you want to track."
-        >
-            <vue-bootstrap-typeahead
-                v-model="companySymbol"
-                placeholder="Enter company symbol"
-                @hit="selectCompany($event)"
-            >
-                <template v-slot:suggestion="{ data, htmlText }">
-                    <span class="d-flex justify-content-between align-center">
-                        <span v-html="htmlText"></span
-                        ><small>{{ data.name }}</small>
-                    </span>
-                </template>
-            </vue-bootstrap-typeahead>
-        </b-form-group>
-        <b-button
-            :disabled="!companySymbol.length"
-            class="mt-4"
-            type="submit"
-            variant="primary"
-            >Track</b-button
-        >
-    </b-form>
+    <b-col cols="3">
+        <data-model :filter="companySymbol">
+            <template v-slot="{ addCompany, companies, selectCompany }">
+                <b-form @submit.prevent="addCompany">
+                    <b-form-group
+                        id="input-group-1"
+                        label="Company symbol:"
+                        label-for="company-symbol"
+                        description="Provide the stock exchange symbol of a company you want to track."
+                    >
+                        <vue-bootstrap-typeahead
+                            v-model="companySymbol"
+                            :data="companies"
+                            :serializer="(company) => company.symbol"
+                            placeholder="Enter company symbol"
+                            @hit="selectCompany($event)"
+                        >
+                            <template v-slot:suggestion="{ data, htmlText }">
+                                <span
+                                    class="d-flex justify-content-between align-center"
+                                >
+                                    <span v-html="htmlText"></span
+                                    ><small>{{ data.name }}</small>
+                                </span>
+                            </template>
+                        </vue-bootstrap-typeahead>
+                    </b-form-group>
+                    <b-button
+                        :disabled="!companySymbol.length"
+                        class="mt-4"
+                        type="submit"
+                        variant="primary"
+                        >Track</b-button
+                    >
+                </b-form>
+            </template>
+        </data-model>
+    </b-col>
 </template>
 
 <script>
+import DataModel from "./data-model";
+
 export default {
     name: "CompanyAdd",
+    components: {
+        DataModel,
+    },
     data() {
         return {
             companySymbol: "",
