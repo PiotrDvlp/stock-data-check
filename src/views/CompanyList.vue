f136d41f14ec4336c58ceaf57ac87d47b490ce10
Piotr Strzelecki <strzelecki.piotr@protonmail.com>
2022-06-20 15:50:35 +0200

    feature: Add company list item

--- src/views/CompanyList.vue
+++ src/views/CompanyList.vue
@@ -1,5 +1,33 @@
 <template>
-    <div>Company List</div>
+    <ul>
+        <li class="company-data d-flex flex-row">
+            <div class="company-data__logo">
+                <img src="https://via.placeholder.com/64x64" />
+            </div>
+            <div class="company-data__details">
+                <div class="company-data__name">
+                    <span
+                        class="company-data__info company-data__info--bold company-data__info--big"
+                        >Alphabet Inc.</span
+                    >
+                    <span class="company-data__info">GOOG</span>
+                    <span class="company-data__info">abc.xyz</span>
+                </div>
+                <div class="company-data__local">
+                    <span class="company-data__info">United States</span>
+                    <span class="company-data__info">09.30 - 16.00</span>
+                    <span class="company-data__info">UTC-5</span>
+                </div>
+                <div class="company-data__stock">
+                    <span class="company-data__info company-data__info--bold"
+                        >131.73 USD</span
+                    >
+                    <span class="company-data__info">−13.73 (1.32%)</span>
+                    <span class="company-data__info">Closed: 2018-11-23</span>
+                </div>
+            </div>
+        </li>
+    </ul>
 </template>
 
 <script>
@@ -8,3 +36,23 @@ export default {
     components: {},
 };
 </script>
+<style lang="scss" scoped>
+.company-data {
+    &__logo {
+        margin-right: 10px;
+    }
+
+    &__info {
+        padding: 0 4px;
+        font-size: 14px;
+
+        &--big {
+            font-size: 18px;
+        }
+
+        &--bold {
+            font-weight: bold;
+        }
+    }
+}
+</style>
