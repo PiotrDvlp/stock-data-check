2535b73ec7a1f25807e80b023ed5373363ecf40d
Piotr Strzelecki <strzelecki.piotr@protonmail.com>
2022-06-20 12:22:08 +0200

    feature: Add layout and navbar components

---
+++ src/components/TheLayout.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="layout">
+        <the-navbar></the-navbar>
+        <main class="layout__main pt-4">
+            <b-container fluid>
+                <b-row>
+                    <b-col>
+                        <h1 class="h4 mb-4">Track new company</h1>
+                    </b-col>
+                </b-row>
+                <b-row>
+                    <slot></slot>
+                </b-row>
+            </b-container>
+        </main>
+    </div>
+</template>
+
+<script>
+import TheNavbar from "@/components/TheNabvar";
+
+export default {
+    name: "TheLayout",
+    components: {
+        TheNavbar,
+    },
+};
+</script>
+
+<style></style>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="layout__header">
+        <b-navbar toggleable="sm" type="light" variant="light">
+            <b-container fluid>
+                <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+
+                <b-navbar-brand>Stock Tracker</b-navbar-brand>
+
+                <b-collapse id="nav-text-collapse" is-nav>
+                    <b-navbar-nav>
+                        <b-nav-item href="#">Track New Company</b-nav-item>
+                        <b-nav-item href="#">Companies</b-nav-item>
+                    </b-navbar-nav>
+                </b-collapse>
+            </b-container>
+        </b-navbar>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "TheNavbar",
+};
+</script>
+
+<style></style>
