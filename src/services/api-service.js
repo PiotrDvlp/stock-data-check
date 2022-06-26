968233c4ea6a2f58b3148a1456a5d2a37a8dde47
Piotr Strzelecki <strzelecki.piotr@protonmail.com>
2022-06-26 19:14:10 +0200

    fix: Create api service

---
+++ src/services/api-service.js
@@ -0,0 +1,29 @@
+const apiKey = process.env.VUE_APP_ALPHA_VANTAGE_KEY;
+
+class ApiService {
+    constructor() {
+        const service = this.$http.create();
+
+        this.service = service;
+    }
+
+    getCompanyBySymbol(symbol) {
+        return this.service.get(
+            `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${symbol}&apikey=${apiKey}`
+        );
+    }
+
+    getClearbitData(companyName) {
+        return this.service.get(
+            `https://autocomplete.clearbit.com/v1/companies/suggest?query=${companyName}`
+        );
+    }
+
+    getAlphavData(symbol) {
+        return this.service.get(
+            `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${apiKey}`
+        );
+    }
+}
+
+export default new ApiService();
