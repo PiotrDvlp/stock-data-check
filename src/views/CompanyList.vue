df1663c4f72c9a51f631b49996ed3499ea0aa71e
Piotr Strzelecki <strzelecki.piotr@protonmail.com>
2022-06-27 12:39:16 +0200

    feature: Add CTA to template

--- src/views/CompanyList.vue
+++ src/views/CompanyList.vue
@@ -2,14 +2,26 @@
     <b-col cols="4">
         <company-list-model>
             <template v-slot="{ chosenCompanies, removeFromList }">
-                <ul class="company__list">
-                    <company-list-item
-                        v-for="company in chosenCompanies"
-                        :company="company"
-                        :key="company.uuid"
-                        @remove-company="removeFromList"
-                    ></company-list-item>
-                </ul>
+                <div>
+                    <ul v-if="chosenCompanies.length" class="company__list">
+                        <company-list-item
+                            v-for="company in chosenCompanies"
+                            :company="company"
+                            :key="company.uuid"
+                            @remove-company="removeFromList"
+                        ></company-list-item>
+                    </ul>
+                    <div v-else>
+                        <div>
+                            <p>
+                                There are no companies yet.
+                                <router-link to="/">
+                                    Track your first company.</router-link
+                                >
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </template>
         </company-list-model>
     </b-col>
