190e98f08ee913d4f9371519195f9abd36bd17d6
Piotr Strzelecki <strzelecki.piotr@protonmail.com>
2022-06-27 10:25:33 +0200

    fix: Format data to display

--- src/views/company-list-model.js
+++ src/views/company-list-model.js
@@ -1,5 +1,15 @@
 import ApiService from "../services/api-service";
+import { camelCase } from "lodash";
 
+const formatKey = (str) => camelCase(str.substring(str.indexOf(" ") + 1));
+const renameKeys = (obj) =>
+    Object.keys(obj).reduce(
+        (acc, key) => ({
+            ...acc,
+            ...{ [formatKey(key)]: obj[key] },
+        }),
+        {}
+    );
 const getDataFromStorage = (storageKey) =>
     JSON.parse(localStorage.getItem(storageKey));
 
@@ -27,14 +37,43 @@ export default {
 
             try {
                 const response = await Promise.all(preparedApiCalls);
-                this.setLatestData(response);
+
+                this.formatLatestData(response);
             } catch (error) {
-                console.error(error.response);
+                console.error(error);
             }
         },
-        setLatestData(response) {
-            const data = response.map(({ data }) => data);
-            console.log("data set", data);
+        formatLatestData(response) {
+            const formatedData = response
+                .filter(({ data }) => data["Meta Data"])
+                .reduce((acc, curr) => {
+                    const { symbol, lastRefreshed } = renameKeys(
+                        curr.data["Meta Data"]
+                    );
+                    const { close } = renameKeys(
+                        curr.data["Time Series (5min)"][lastRefreshed]
+                    );
+                    const preparedData = {
+                        close,
+                        lastRefreshed,
+                        symbol,
+                    };
+
+                    acc.push(preparedData);
+
+                    return acc;
+                }, []);
+
+            this.setLatestData(formatedData);
+        },
+        setLatestData(formatedData) {
+            this.chosenCompanies = this.chosenCompanies.map((company) => {
+                const updated = formatedData.find(
+                    (updatedData) => updatedData.symbol === company.symbol
+                );
+
+                return updated ? Object.assign(company, updated) : company;
+            });
         },
     },
     render() {
